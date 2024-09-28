9ca3a9f17a1bd6f41bd4256de0fe3de613832b8b
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2024-09-28 15:23:22 +0800

    Add search bar.

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,17 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Header from "./Header";
+import { fn } from "@storybook/test";
 
 const meta = {
   title: "Components/Header",
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: "fullscreen",
+  args: {
+    onSearchRequested: fn(),
   },
-  args: {},
 } satisfies Meta<typeof Header>;
 
 export default meta;
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,19 +1,33 @@
+import { ArrowRight, Search } from "@mui/icons-material";
 import "./fonts";
-import { Stack, Breakpoint, Container, Typography } from "@mui/material";
+import {
+  Stack,
+  Breakpoint,
+  Container,
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { useState } from "react";
 
 export interface HeaderProps {
   maxWidth?: Breakpoint | false;
   title?: string;
   aside?: string;
+  onSearchRequested: (query: string) => void;
 }
 
 export default function Header({
   maxWidth = "lg",
   title = "SEPADU",
   aside = "Jambatan Perkhidmatan Awam",
+  onSearchRequested,
 }: HeaderProps) {
+  const [query, setQuery] = useState("");
+
   return (
-    <Container maxWidth={maxWidth}>
+    <Container maxWidth={maxWidth} sx={{ p: 3 }}>
       <Stack
         direction="row"
         sx={{
@@ -22,7 +36,45 @@ export default function Header({
         }}
       >
         <Typography>{title}</Typography>
-        <Typography>{aside}</Typography>
+        <Stack
+          direction="row"
+          sx={{
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <TextField
+            sx={{
+              mr: 3,
+            }}
+            label="Search"
+            value={query}
+            onChange={(e) => {
+              e.preventDefault();
+              setQuery(e.target.value);
+            }}
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Search></Search>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => onSearchRequested(query)}>
+                      <ArrowRight></ArrowRight>
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+            onKeyUp={(e) => {
+              if (e.code === "Enter") onSearchRequested(query);
+            }}
+          ></TextField>
+          <Typography>{aside}</Typography>
+        </Stack>
       </Stack>
     </Container>
   );
